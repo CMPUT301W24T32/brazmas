774c42e999d002ad1d9ad14b8e52dd8d0879abc4
Abbinash Ranjitkar <abbinashranjitkar@gmail.com>
2024-02-25 19:47:51 -0700

    Initial commit

---
+++ app/src/main/java/com/CMPUT301W24T32/brazmascheckin/MainActivity.java
@@ -0,0 +1,14 @@
+package com.CMPUT301W24T32.brazmascheckin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
