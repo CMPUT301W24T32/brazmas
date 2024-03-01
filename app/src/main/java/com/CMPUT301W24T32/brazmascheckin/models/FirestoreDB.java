5d14d8e0ab8c742df6c52fee8b49596c21d8185f
Abbinash Ranjitkar <abbinashranjitkar@gmail.com>
2024-02-29 23:52:58 -0700

    Finished skeleton for Event.java, moving focus to controller/repository layer

--- app/src/main/java/com/CMPUT301W24T32/brazmascheckin/models/FirestoreDB.java
+++ app/src/main/java/com/CMPUT301W24T32/brazmascheckin/models/FirestoreDB.java
@@ -1,5 +1,9 @@
 package com.CMPUT301W24T32.brazmascheckin.models;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -26,14 +30,34 @@ public class FirestoreDB {
     }
 
     /**
-     * This method provides a reference to the "attendees" collection
-     * @return a CollectionReference for the "attendees" collection
+     * This method provides a reference to the "users" collection
+     * @return a CollectionReference for the "users" collection
      */
-    public static CollectionReference getAttendeesRef() {
-        return FirebaseFirestore.getInstance().collection("attendees");
+    public static CollectionReference getUsersRef() {
+        return FirebaseFirestore.getInstance().collection("users");
     }
 
-    //TODO: determine data flow for check-in/sign up between Attendee and Event
+
+    public static void updateEvent(Event updatedEvent) {
+        getEventsRef().document(updatedEvent.getID()).set(updatedEvent)
+                .addOnSuccessListener(unused -> {
+
+                })
+                .addOnFailureListener(e -> {
+
+                });
+    }
+
+    public static void updateUser(Attendee user) {
+        //TODO: need to add ID to Attendee
+//        getUsersRef().document(user.getID()).set(user)
+//                .addOnSuccessListener(unused -> {
+//
+//                })
+//                .addOnFailureListener(e -> {
+//
+//                });
+    }
 
     //TODO: include static methods to create/update/destroy database entry attributes
 }
