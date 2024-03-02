d311d78400e399f59ca2ec4711d5d2a0b596c4ae
Abbinash Ranjitkar <abbinashranjitkar@gmail.com>
2024-03-02 11:14:55 -0700

    idk

--- app/src/main/java/com/CMPUT301W24T32/brazmascheckin/models/Attendee.java
+++ app/src/main/java/com/CMPUT301W24T32/brazmascheckin/models/Attendee.java
@@ -2,44 +2,111 @@ package com.CMPUT301W24T32.brazmascheckin.models;
 
 import java.util.ArrayList;
 
+/**
+ * This class is a representation of the Attendee entity.
+ */
 public class Attendee {
     private String firstName;
     private String lastName;
-    private ArrayList<Event> signedUpEvents;
+    private ArrayList<String> signedUpEvents;
+    private String ID;
 
-    //TODO: add image profilePicture
+    private String profilePicture;
 
-    public Attendee(String firstName, String lastName, ArrayList<Event> signedUpEvents){
+
+    /**
+     * Constructs a new instance of the Attendee class with the full information for an attendee.
+     * @param ID Identification assigned by FirestoreDB
+     * @param firstName First name of attendee
+     * @param lastName Last name of attendee
+     * @param signedUpEvents Events the attendee has signed up for
+     * @param profilePictureID Reference to the image
+     */
+
+    public Attendee(String ID, String firstName, String lastName, ArrayList<String> signedUpEvents,
+                    String profilePictureID){
+        this.ID = ID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.signedUpEvents = signedUpEvents;
+        this.profilePicture = profilePictureID;
+    }
+
+    /**
+     * Constructs a new instance of the Attendee class with the mandatory information for an attendee.
+     * @param ID Identification assigned by FirestoreDB
+     * @param firstName First name of attendee
+     * @param lastName Last name of attendee
+     * @param signedUpEvents Events the attendee has signed up for
+     */
+    public Attendee(String ID, String firstName, String lastName, ArrayList<String>
+                    signedUpEvents) {
+        this.ID = ID;
         this.firstName = firstName;
         this.lastName = lastName;
         this.signedUpEvents = signedUpEvents;
     }
 
+    /**
+     * Constructs a new instance of the Attendee class as required by Firestore
+     */
+    public Attendee() {
+
+    }
+
+    /**
+     * Getter for the first name of the attendee
+     * @return the first name of the attendee
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Setter for the first name of the attendee
+     * @param firstName new first name of the attendee
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+     * Getter for the last name of the attendee
+     * @return the last name of the attendee
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * Setter for the last name of the attendee
+     * @param lastName new last name of the attendee
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
-    public ArrayList<Event> getSignedUpEvents() {
+    /**
+     * This method provides the list of events the attendee has signed-up to attend
+     * @return the list of events the attendee has signed-up to event
+     */
+    public ArrayList<String> getSignedUpEvents() {
         return signedUpEvents;
     }
 
-    public void setSignedUpEvents(ArrayList<Event> signedUpEvents) {
+    /**
+     * Setter for the signed-up events list
+     * @param signedUpEvents List of events the attendee has signed-up to attend
+     */
+    public void setSignedUpEvents(ArrayList<String> signedUpEvents) {
         this.signedUpEvents = signedUpEvents;
     }
 
-    public void signUpEvent(Event e){
+    /**
+     * This method "signs-up" the attendee to the event
+     * @param e the reference ID of the event
+     */
+    public void signUpEvent(String e){
         if (!signedUpEvents.contains(e)) {
             signedUpEvents.add(e);
         }
@@ -48,13 +115,49 @@ public class Attendee {
         }
     }
 
-    public void unSignUpEvent(Event e){
+    /**
+     * This method "un-signs-up" the attendee from an event
+     * @param e the reference ID of the event
+     */
+    public void unSignUpEvent(String e){
         if (signedUpEvents.contains(e)) {
             signedUpEvents.remove(e);
         }
         else{
             throw new IllegalArgumentException();
         }
+    }
+
+    /**
+     * Getter for the ID of the attendee
+     * @return the identification string assigned by FirestoreDB
+     */
+    public String getID() {
+        return ID;
+    }
+
+    /**
+     * Setter for the ID of the event
+     * @param ID the identification string assigned by FirestoreDB
+     */
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    /**
+     * Getter for the attendee's profile picture
+     * @return reference ID for the image containing the profile picture
+     */
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+
+    /**
+     * Setter for the attendee's profile picture
+     * @param profilePicture reference ID for the image containing the profile picture
+     */
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
     }
 
     // TODO: public void setProfilePicture(Image picture)
---
+++ app/src/main/java/com/CMPUT301W24T32/brazmascheckin/models/FirestoreDB.java
@@ -0,0 +1,39 @@
+package com.CMPUT301W24T32.brazmascheckin.models;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+/**
+ * This class provides access to Firebase database instances and collection references
+ * through static methods.
+ */
+public class FirestoreDB {
+
+    /**
+     * This method provides an instance of the Firestore database.
+     * @return the FirebaseFirestore database instance
+     */
+    public static FirebaseFirestore getDatabaseInstance() {
+        return FirebaseFirestore.getInstance();
+    }
+
+    /**
+     * This method provides a reference to the "events" collection
+     * @return a CollectionReference for the "events" collection
+     */
+    public static CollectionReference getEventsRef() {
+        return getDatabaseInstance().collection("events");
+    }
+
+    /**
+     * This method provides a reference to the "users" collection
+     * @return a CollectionReference for the "users" collection
+     */
+    public static CollectionReference getUsersRef() {
+        return getDatabaseInstance().collection("users");
+    }
+}
