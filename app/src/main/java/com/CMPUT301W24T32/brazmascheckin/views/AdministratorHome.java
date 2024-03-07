a135bd4c4a3f16689c50d3b403abeb55afed7b87
Ria Ahir <ria.ahir1@gmail.com>
2024-03-06 21:31:23 -0700

    Administrator home page

--- app/src/main/java/com/CMPUT301W24T32/brazmascheckin/views/AdministratorHome.java
+++ app/src/main/java/com/CMPUT301W24T32/brazmascheckin/views/AdministratorHome.java
@@ -1,16 +1,46 @@
 package com.CMPUT301W24T32.brazmascheckin.views;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
+//import com.CMPUT301W24T32.brazmascheckin.AttendeeViewEventFragment;
 import com.CMPUT301W24T32.brazmascheckin.R;
+import com.CMPUT301W24T32.brazmascheckin.helper.Date;
+import com.CMPUT301W24T32.brazmascheckin.helper.EventRecyclerViewAdapter;
+import com.CMPUT301W24T32.brazmascheckin.models.Event;
+import com.CMPUT301W24T32.brazmascheckin.models.FirestoreDB;
+import com.CMPUT301W24T32.brazmascheckin.views.AttendeeViewEventFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
 
 public class AdministratorHome extends AppCompatActivity {
+
+    private ArrayList<Event> eventDataList;
+    private EventRecyclerViewAdapter eventRecyclerViewAdapter;
+
+    private RecyclerView eventRecyclerView;
+
+    private CollectionReference eventsRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_administrator_home);
+
+        configureViews();
+        configureControllers();
     }
 }
