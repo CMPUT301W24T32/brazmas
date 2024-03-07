e4623208be44b737af2f3ca6e3ae74b3854fe38a
z-codeit <zaaradsouza091@gmail.com>
2024-03-07 10:32:50 -0700

    added CaptureAct, modified Camera Activity to include scannign functionality

--- app/src/main/java/com/CMPUT301W24T32/brazmascheckin/views/CameraActivity.java
+++ app/src/main/java/com/CMPUT301W24T32/brazmascheckin/views/CameraActivity.java
@@ -1,29 +1,41 @@
 package com.CMPUT301W24T32.brazmascheckin.views;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
+import com.CMPUT301W24T32.brazmascheckin.MainActivity;
 import com.CMPUT301W24T32.brazmascheckin.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
 
 /**
  * Camera activity-> made for testing navigation
  */
 public class CameraActivity extends AppCompatActivity {
 
+    Button btn_scan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_camera);
+
+        //initialize scan button
+        btn_scan = findViewById(R.id.qr_code_scan_btn);
+        btn_scan.setOnClickListener(v-> {
+                scanCode();
+                });
+
         // Allows the app to switch between activities
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setSelectedItemId(R.id.bottom_camera);
@@ -55,4 +67,25 @@ public class CameraActivity extends AppCompatActivity {
         });
 
     }
+
+    public void scanCode() {
+        ScanOptions options = new ScanOptions();
+        options.setPrompt("volume up to flash on");
+        options.setBeepEnabled(true);
+        options.setOrientationLocked(true);
+        options.setCaptureActivity(CaptureAct.class);
+    }
+
+    ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(), result -> {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Result");
+        builder.setMessage(result.getContents());
+        builder.setMessage(result.getContents());
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        }).show();
+    });
 }
---
+++ app/src/main/java/com/CMPUT301W24T32/brazmascheckin/views/CaptureAct.java
@@ -0,0 +1,8 @@
+package com.CMPUT301W24T32.brazmascheckin.views;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class CaptureAct extends CaptureActivity {
+
+
+}
