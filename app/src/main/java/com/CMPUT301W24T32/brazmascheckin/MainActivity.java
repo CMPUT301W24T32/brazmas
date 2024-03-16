c7b387cff3766ba58103ce82d83c6010ddff390f
Abbinash Ranjitkar <abbinashranjitkar@gmail.com>
2024-03-15 22:53:46 -0600

    Added Controller for Admin interactions

--- app/src/main/java/com/CMPUT301W24T32/brazmascheckin/MainActivity.java
+++ app/src/main/java/com/CMPUT301W24T32/brazmascheckin/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.CMPUT301W24T32.brazmascheckin.controllers.AdminController;
+import com.CMPUT301W24T32.brazmascheckin.controllers.AdminGetListener;
 import com.CMPUT301W24T32.brazmascheckin.controllers.UserController;
 import com.CMPUT301W24T32.brazmascheckin.controllers.UserGetListener;
 import com.CMPUT301W24T32.brazmascheckin.controllers.UserSetListener;
@@ -25,9 +27,11 @@ import java.util.ArrayList;
  * It handles the device verification process and redirects users based on their status.
  */
 
-public class MainActivity extends AppCompatActivity implements UserGetListener, UserSetListener {
+public class MainActivity extends AppCompatActivity implements UserGetListener, UserSetListener,
+        AdminGetListener {
 
     private UserController userController;
+    private AdminController adminController;
     private String deviceID;
 
     @Override
@@ -38,34 +42,12 @@ public class MainActivity extends AppCompatActivity implements UserGetListener, 
         // getting device ID and storing it in a string
         deviceID = DeviceID.getDeviceID(this);
         userController = new UserController(this);
+        adminController = new AdminController(this);
 
         // control flow of different types of users
-        verifyAdministratorStatus(deviceID);
-
-//        verifyUserStatus(deviceID);
+        adminController.getAdmin(deviceID, this);
         userController.getUser(deviceID, this);
     }
-
-    /**
-     * This method determines if the device belongs to an administrator, and redirects them accordingly.
-     * @param deviceID the device ID
-     */
-    private void verifyAdministratorStatus(String deviceID) {
-        CollectionReference adminsRef = FirestoreDB.getAdminsRef();
-        adminsRef.document(deviceID).get()
-                .addOnSuccessListener(documentSnapshot -> {
-                    // if the device ID is registered as an administrator
-                    if(documentSnapshot.exists()) {
-                        Intent intent = new Intent(MainActivity.this, AdministratorHome.class);
-                        startActivity(intent);
-                        finish();
-                    }
-                })
-                .addOnFailureListener(e -> {
-                    // TODO: handle failure
-                });
-    }
-
 
     @Override
     public void onUserGetSuccess(User user) {
@@ -109,5 +91,17 @@ public class MainActivity extends AppCompatActivity implements UserGetListener, 
     @Override
     public void onUserSetFailure() {
         Toast.makeText(this, "Unable to create profile", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onAdminGetSuccess() {
+        Intent intent = new Intent(MainActivity.this, AdministratorHome.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public void onAdminGetFailure(Exception e) {
+
     }
 }
---
+++ app/src/main/java/com/CMPUT301W24T32/brazmascheckin/controllers/AdminController.java
@@ -0,0 +1,28 @@
+package com.CMPUT301W24T32.brazmascheckin.controllers;
+
+import android.content.Context;
+
+import com.CMPUT301W24T32.brazmascheckin.models.FirestoreDB;
+import com.google.firebase.firestore.CollectionReference;
+
+public class AdminController {
+    private Context context;
+    private CollectionReference adminsRef;
+
+    public AdminController(Context context) {
+        this.context = context;
+        adminsRef = FirestoreDB.getAdminsRef();
+    }
+
+    public void getAdmin(String ID, AdminGetListener listener) {
+        adminsRef.document(ID).get()
+                .addOnSuccessListener(documentSnapshot -> {
+                    if(documentSnapshot != null) {
+                        listener.onAdminGetSuccess();
+                    } else {
+                        listener.onAdminGetFailure(null);
+                    }
+                })
+                .addOnFailureListener(listener::onAdminGetFailure);
+    }
+}
