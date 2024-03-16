cf94e674a78b40feffce99fe6aaa4c4f999f6591
Saaheb <saaheb@ualberta.ca>
2024-03-15 22:13:54 -0600

    name displayed on profile activty

--- app/src/main/java/com/CMPUT301W24T32/brazmascheckin/views/ProfileActivity.java
+++ app/src/main/java/com/CMPUT301W24T32/brazmascheckin/views/ProfileActivity.java
@@ -2,20 +2,36 @@ package com.CMPUT301W24T32.brazmascheckin.views;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.CMPUT301W24T32.brazmascheckin.R;
+import com.CMPUT301W24T32.brazmascheckin.helper.DeviceID;
+import com.CMPUT301W24T32.brazmascheckin.models.Event;
+import com.CMPUT301W24T32.brazmascheckin.models.FirestoreDB;
+import com.CMPUT301W24T32.brazmascheckin.models.User;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 /**
  * Profile activity made for testing navigation
  */
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private ImageView profilePicture;
+    private TextView userName;
+    private CollectionReference usersRef;
+    private DocumentReference userDoc;
     /**
      *This method initializes the Profile activity.
      * @param savedInstanceState If the activity is being re-initialized after
@@ -31,6 +47,9 @@ public class ProfileActivity extends AppCompatActivity {
         // Allows the app to switch between activities
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setSelectedItemId(R.id.bottom_profile);
+
+        configureViews();
+        configureControllers();
         bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             /**
              * This method determines something from navigation bar has been selected or not.
@@ -62,6 +81,29 @@ public class ProfileActivity extends AppCompatActivity {
 
             }
         });
+    }
 
+    /**
+     * configures the views for profile activity
+     */
+    public void configureViews(){
+        profilePicture = findViewById(R.id.profile_picture);
+        userName = findViewById(R.id.name_tv);
+        usersRef = FirestoreDB.getUsersRef();
+        String deviceID = DeviceID.getDeviceID(this);
+        userDoc = usersRef.document(deviceID);
+    }
+
+    /**
+     * configure controllers for profile activity
+     */
+    public void configureControllers(){
+        userDoc.get().addOnSuccessListener(documentSnapshot -> {
+            User user = documentSnapshot.toObject(User.class);
+            String firstName = user.getFirstName();
+            String lastName = user.getLastName();
+            String fullName = firstName + " " + lastName;
+            userName.setText(fullName);
+        });
     }
 }
