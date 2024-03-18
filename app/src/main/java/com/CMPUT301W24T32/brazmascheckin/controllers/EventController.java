7aa48a09bbae6384888c850f3b13f673436234d7
z-codeit <zaaradsouza091@gmail.com>
2024-03-18 15:58:39 -0600

    set up helper class to extract unused QR codes

--- app/src/main/java/com/CMPUT301W24T32/brazmascheckin/controllers/EventController.java
+++ app/src/main/java/com/CMPUT301W24T32/brazmascheckin/controllers/EventController.java
@@ -2,24 +2,15 @@ package com.CMPUT301W24T32.brazmascheckin.controllers;
 
 import android.content.Context;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.FragmentActivity;
-
 import com.CMPUT301W24T32.brazmascheckin.models.Event;
 import com.CMPUT301W24T32.brazmascheckin.models.FirestoreDB;
-import com.CMPUT301W24T32.brazmascheckin.models.User;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.ListenerRegistration;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Controller responsible for managing interactions with events in the Firestore Database.
@@ -183,4 +174,22 @@ public class EventController {
         });
     }
 
+    public List<String> getAllEventQRCodeIDs() {
+        final List<String> qrCodeIDs = new ArrayList<>();
+
+        eventsRef.get().addOnSuccessListener(queryDocumentSnapshots -> {
+            for (DocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                Event event = documentSnapshot.toObject(Event.class);
+                if (event != null) {
+                    String qrCodeID = event.getQRCode();
+                    if (qrCodeID != null && !qrCodeID.isEmpty()) {
+                        qrCodeIDs.add(qrCodeID);
+                    }
+                }
+            }
+        }).addOnFailureListener(e -> {
+            // Handle failure
+        });
+        return qrCodeIDs;
+    }
 }
