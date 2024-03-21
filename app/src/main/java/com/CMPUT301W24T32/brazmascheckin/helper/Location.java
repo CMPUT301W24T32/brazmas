b30b224948106beebe9ef75df8b4c0574cb6464b
Abbinash Ranjitkar <abbinashranjitkar@gmail.com>
2024-03-21 13:12:01 -0600

    Helper classes to store Location, pair of userID and location

---
+++ app/src/main/java/com/CMPUT301W24T32/brazmascheckin/helper/Location.java
@@ -0,0 +1,60 @@
+package com.CMPUT301W24T32.brazmascheckin.helper;
+
+public class Location {
+    private double latitude;
+    private double longitude;
+
+    /**
+     * Constructs a new Location object as required by Firebase.
+     */
+    public Location() {
+
+    }
+
+    /**
+     * Constructs a new Location object with the specified latitude and longitude.
+     *
+     * @param latitude  the latitude coordinate of the location
+     * @param longitude the longitude coordinate of the location
+     */
+    public Location(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    /**
+     * Returns the latitude coordinate of the location.
+     *
+     * @return the latitude coordinate
+     */
+    public double getLatitude() {
+        return latitude;
+    }
+
+    /**
+     * Sets the latitude coordinate of the location.
+     *
+     * @param latitude the latitude coordinate to set
+     */
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    /**
+     * Returns the longitude coordinate of the location.
+     *
+     * @return the longitude coordinate
+     */
+    public double getLongitude() {
+        return longitude;
+    }
+
+    /**
+     * Sets the longitude coordinate of the location.
+     *
+     * @param longitude the longitude coordinate to set
+     */
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+}
---
+++ app/src/main/java/com/CMPUT301W24T32/brazmascheckin/helper/UserLocationPair.java
@@ -0,0 +1,63 @@
+package com.CMPUT301W24T32.brazmascheckin.helper;
+
+/**
+ * Represents a pair consisting of a user ID and their check-in location.
+ */
+public class UserLocationPair {
+    private String userID;
+    private Location userCheckInLocation;
+
+    /**
+     * Constructs a new UserLocationPair object as required by Firebase.
+     */
+    public UserLocationPair() {
+
+    }
+
+    /**
+     * Constructs a new UserLocationPair object with the specified user ID and check-in location.
+     *
+     * @param userID             the user ID
+     * @param userCheckInLocation the check-in location of the user
+     */
+    public UserLocationPair(String userID, Location userCheckInLocation) {
+        this.userID = userID;
+        this.userCheckInLocation = userCheckInLocation;
+    }
+
+    /**
+     * Returns the user ID.
+     *
+     * @return the user ID
+     */
+    public String getUserID() {
+        return userID;
+    }
+
+    /**
+     * Sets the user ID.
+     *
+     * @param userID the user ID to set
+     */
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    /**
+     * Returns the check-in location of the user.
+     *
+     * @return the check-in location
+     */
+    public Location getUserCheckInLocation() {
+        return userCheckInLocation;
+    }
+
+    /**
+     * Sets the check-in location of the user.
+     *
+     * @param userCheckInLocation the check-in location to set
+     */
+    public void setUserCheckInLocation(Location userCheckInLocation) {
+        this.userCheckInLocation = userCheckInLocation;
+    }
+}
