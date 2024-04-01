15d08ac8b0d1ef38c75e1647e811a85b44446756
Abbinash Ranjitkar <abbinashranjitkar@gmail.com>
2024-03-31 20:24:25 -0600

    First test works

--- app/src/androidTest/java/com/CMPUT301W24T32/brazmascheckin/EventControllerIntegrationTest.java
+++ app/src/androidTest/java/com/CMPUT301W24T32/brazmascheckin/EventControllerIntegrationTest.java
@@ -7,6 +7,8 @@ import androidx.test.core.app.ApplicationProvider;
 import com.CMPUT301W24T32.brazmascheckin.controllers.AddSuccessListener;
 import com.CMPUT301W24T32.brazmascheckin.controllers.EventController;
 import com.CMPUT301W24T32.brazmascheckin.models.Event;
+import com.CMPUT301W24T32.brazmascheckin.models.FirestoreDB;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
@@ -23,38 +25,48 @@ import org.mockito.junit.MockitoJUnitRunner;
 @RunWith(MockitoJUnitRunner.class)
 public class EventControllerIntegrationTest {
 
-    @Mock
+//    @Mock
     private FirebaseFirestore mockDatabase;
-    @Mock
+//    @Mock
     private CollectionReference mockCollectionRef;
-    @Mock
+//    @Mock
     private DocumentReference mockDocumentRef;
 
-    @InjectMocks
+//    @InjectMocks
     private EventController eventController;
+    private FirestoreDB mockDB;
 
     @Before
     public void setUp() {
-        MockitoAnnotations.initMocks(this);
-//        mockDatabase = mock(FirebaseFirestore.class);
-//        mockCollectionRef = mock(CollectionReference.class);
-//        mockDocumentRef = mock(DocumentReference.class);
+//        MockitoAnnotations.initMocks(this);
+        mockDatabase = mock(FirebaseFirestore.class);
+        mockCollectionRef = mock(CollectionReference.class);
+        mockDocumentRef = mock(DocumentReference.class);
+        mockDB = mock(FirestoreDB.class);
 //
 //        FirebaseApp.initializeApp(ApplicationProvider.getApplicationContext());
 
-        when(mockDatabase.collection("events")).thenReturn(mockCollectionRef);
+        when(mockDatabase.collection(anyString())).thenReturn(mockCollectionRef);
         when(mockCollectionRef.document(anyString())).thenReturn(mockDocumentRef);
 
-//        eventController = new EventController(mockDatabase);
+
+        eventController = new EventController(mockDatabase);
     }
 
     @Test
     public void testAddEvent_Success() {
         Event mockEvent = mock(Event.class);
         when(mockDocumentRef.getId()).thenReturn("mock_event_id");
+        Task mockTask = mock(Task.class);
+        when(mockCollectionRef.add(any(Event.class))).thenReturn(mockTask);
+        when(mockTask.addOnSuccessListener(any())).thenReturn(mockTask);
+        when(mockTask.addOnFailureListener(any())).thenReturn(mockTask);
 
         eventController.addEvent(mockEvent, object -> {
             assert object.equals("mock_event_id");
         }, null);
+
+
+
     }
 }
