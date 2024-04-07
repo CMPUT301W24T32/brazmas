382a0c3524e3c2e12826e921e2f295adba61fc7d
Ria Ahir <ria.ahir1@gmail.com>
2024-04-06 19:03:12 -0600

    Changes to delete event test for admin.

--- app/src/androidTest/java/com/CMPUT301W24T32/brazmascheckin/AdminDeleteEventTest.java
+++ app/src/androidTest/java/com/CMPUT301W24T32/brazmascheckin/AdminDeleteEventTest.java
@@ -1,15 +1,18 @@
 package com.CMPUT301W24T32.brazmascheckin;
 
+import androidx.test.core.app.ActivityScenario;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.espresso.Espresso;
 import androidx.test.espresso.InjectEventSecurityException;
 import androidx.test.espresso.UiController;
 import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.assertion.ViewAssertions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
+import static org.mockito.ArgumentMatchers.any;
 
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
@@ -18,7 +21,6 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
-
 import static org.hamcrest.Matchers.instanceOf;
 
 import android.os.SystemClock;
@@ -26,11 +28,14 @@ import android.view.MotionEvent;
 import android.view.View;
 
 import com.CMPUT301W24T32.brazmascheckin.controllers.AdminController;
+import com.CMPUT301W24T32.brazmascheckin.controllers.DeleteFailureListener;
+import com.CMPUT301W24T32.brazmascheckin.controllers.DeleteSuccessListener;
 import com.CMPUT301W24T32.brazmascheckin.controllers.EventController;
 import com.CMPUT301W24T32.brazmascheckin.controllers.GetFailureListener;
 import com.CMPUT301W24T32.brazmascheckin.controllers.GetSuccessListener;
 import com.CMPUT301W24T32.brazmascheckin.controllers.SnapshotListener;
 import com.CMPUT301W24T32.brazmascheckin.controllers.UserController;
+import com.CMPUT301W24T32.brazmascheckin.helper.Date;
 import com.CMPUT301W24T32.brazmascheckin.helper.DeviceID;
 import com.CMPUT301W24T32.brazmascheckin.models.Admin;
 import com.CMPUT301W24T32.brazmascheckin.models.Event;
@@ -51,9 +56,12 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.osmdroid.views.MapView;
+import static org.mockito.Mockito.*;
+import org.mockito.Mockito;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import androidx.test.filters.LargeTest;
 
@@ -66,62 +74,43 @@ import org.junit.Rule;
 @RunWith(AndroidJUnit4.class)
 public class AdminDeleteEventTest {
 
-    @Rule
-    public ActivityScenarioRule<AdministratorHome> scenario = new ActivityScenarioRule<>(AdministratorHome.class);
-
-    private AdminController adminController;
-    private EventController eventController;
-    private Admin admin = new Admin(DeviceID.getDeviceID(
-            ApplicationProvider.getApplicationContext()
-    ), "test", "device", new ArrayList<>(), false, new ArrayList<>());
-
-    @Before
-    public void setUp() {
-        adminController = new AdminController(FirestoreDB.getDatabaseInstance());
-        eventController = new EventController(FirestoreDB.getDatabaseInstance());
-        adminController.setAdmin(admin, null, null);
-    }
-
-    @After
-    public void cleanUp() {
-        adminController.getAdmin(admin.getID(), object -> admin = object, null);
-
-        // need to add other stuff?
-
-        // do I need this?
-        //adminController.deleteAdmin(admin, null, null);  // do I need to delete this?
-    }
-
     @Test
-    public void testShowAllEvents() {
-        Espresso.onView(ViewMatchers.withId(R.id.event_text_view_admin))
-                .perform(ViewActions.typeText("Events"));
+    public void testEventDeletion() {
 
-        try {
-            Thread.sleep(5000);
-        } catch (Exception e) {
+        // mock EventController
+        EventController eventController = Mockito.mock(EventController.class);
 
-        }
+        // create mock event to be deleted
+        //Event mockEvent = new Event("event_id_to_be_deleted", "Mock Event", "Mock Description", "Organizer ID");
 
+        // create mock event to be deleted
+        Date eventDate = new Date();
+        HashMap<String, Integer> emptyMap = new HashMap<>();
+        ArrayList<String> signUps = new ArrayList<>();
+        Event mockEvent = new Event("event_id", "Mock Event", eventDate, "Mock Description", emptyMap, signUps, 1, null, null, null, "Organizer ID", false, null, null, null);
 
+        // launch AdministratorHome activity, mock event will be displayed
+        ActivityScenario<AdministratorHome> activityScenario = ActivityScenario.launch(AdministratorHome.class);
+
+        activityScenario.onActivity(activity -> {
+            // verify the event is displayed in the UI
+            Espresso.onView(withText("Mock Event")).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+
+            // click on the mock event to view its details
+            Espresso.onView(withText("Mock Event")).perform(ViewActions.click());
+
+            // click on the "Delete Event" button to delete the event
+            Espresso.onView(withText("Delete Event")).perform(ViewActions.click());
+
+            // wrap the verification inside runOnUiThread
+            /*activity.runOnUiThread(() -> {
+                // verify that the event deletion is triggered in the controller
+                verify(eventController).deleteEvent(any(String.class), any(DeleteSuccessListener.class), any(DeleteFailureListener.class));
+            });*/
+
+            // verify that the event is no longer displayed in the UI after deletion
+            Espresso.onView(withText("Mock Event")).check(ViewAssertions.doesNotExist());
+
+        });
     }
-
-    @Rule
-    public ActivityScenarioRule<AdministratorHome> scenario = new ActivityScenarioRule<AdministratorHome>(AdministratorHome.class);
-
-    @Test
-    public void adminSelectEventTest() {
-        try {
-            Thread.sleep(3000);
-
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        onView(withText("Mehar cool events")).perform(ViewActions.click());
-        onView(withId(R.id.view_event_delete_btn_admin))
-                .perform(ViewActions.click());
-
-    }
-
 }
