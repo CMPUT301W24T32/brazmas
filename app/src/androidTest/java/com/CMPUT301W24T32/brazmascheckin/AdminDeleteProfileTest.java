55f92ff9ef81dd2cfec03988928c6ee7c4706e7d
Ria Ahir <ria.ahir1@gmail.com>
2024-04-08 06:10:06 -0600

    Changed logic of AdminDeleteProfileTest.

--- app/src/androidTest/java/com/CMPUT301W24T32/brazmascheckin/AdminDeleteProfileTest.java
+++ app/src/androidTest/java/com/CMPUT301W24T32/brazmascheckin/AdminDeleteProfileTest.java
@@ -1,5 +1,6 @@
 package com.CMPUT301W24T32.brazmascheckin;
 
+import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import androidx.recyclerview.widget.RecyclerView;
@@ -8,16 +9,20 @@ import androidx.test.espresso.Espresso;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.assertion.ViewAssertions;
 import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
 import com.CMPUT301W24T32.brazmascheckin.controllers.UserController;
+import com.CMPUT301W24T32.brazmascheckin.models.FirestoreDB;
 import com.CMPUT301W24T32.brazmascheckin.models.User;
 import com.CMPUT301W24T32.brazmascheckin.views.AdministratorBrowseProfiles;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import org.checkerframework.checker.units.qual.A;
 import org.checkerframework.checker.units.qual.Length;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -25,7 +30,6 @@ import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
 
 
-//TODO: check mock user is on screen before deleting
 /**
  * Test class for deletion of profiles by an administrator.
  * This test ensures that an administrator can successfully delete profiles from the system.
@@ -34,53 +38,16 @@ import java.util.concurrent.atomic.AtomicInteger;
 @RunWith(AndroidJUnit4.class)
 public class AdminDeleteProfileTest {
 
-    /**
-     * Test method to delete profiles.
-     * This method retrieves all profiles from the database and deletes each one.
-     * After deleting all profiles, it creates a mock user profile and verifies its deletion.
-     */
-    @Test
-    public void testDeleteProfileAdmin() {
+    @Rule
+    public ActivityScenarioRule<AdministratorBrowseProfiles> scenario = new ActivityScenarioRule<>(AdministratorBrowseProfiles.class);
 
-        // controllers
-        FirebaseFirestore database = FirebaseFirestore.getInstance();
-        UserController userController = new UserController(database);
+    private UserController userController;
 
-        /*// retrieve all profiles
-        userController.getAllUsers(users -> {
-            // iterate through all users and delete each
-            AtomicInteger counter = new AtomicInteger(users.size());
-            for (User user : users) {
-                userController.deleteUser(user, () -> {
-                    // success listener
-                    if (counter.decrementAndGet() == 0) {
-                        // all users have been deleted, now create mock user
-                        createMockUser(userController);
-                    }
-                }, e -> {
-                    // failure listener
-                    if (counter.decrementAndGet() == 0) {
-                        // all users have been deleted, now create mock user
-                        createMockUser(userController);
-                    }
-                });
-            }
-        }, e -> {
-            // failure for getting all users
-            createMockUser(userController); // launch anyway, perseverance
-        });*/
+    @Before
+    public void setUp() {
+        userController = new UserController(FirestoreDB.getDatabaseInstance());
 
-        // create mock user
-        createMockUser(userController);
-    }
-
-    /**
-     * Creates a mock user that will later be deleted.
-     * @param userController to preform actions on user.
-     */
-    private void createMockUser(UserController userController) {
-        // create mock user
-
+        // create mock profile/user
         // list of signed up events
         ArrayList<String> signedUpEvents = new ArrayList<>();  // it can be empty
 
@@ -88,68 +55,41 @@ public class AdminDeleteProfileTest {
         ArrayList<String> checkedInEvents = new ArrayList<>();  // can be empty
 
         // create mock user
-        User mockUser = new User("000000000", "John", "Doe", signedUpEvents, true, checkedInEvents);
+        User mockUser = new User("000000000", "Mock", "User", signedUpEvents, true, checkedInEvents);
 
         // Add the mock event to Firestore using the EventController
         userController.setUser(mockUser, () -> {
             // Success listener
-            launchAdministratorBrowseProfiles();
         }, failure -> {
             // Failure listener
         });
     }
 
-    /**
-     * Launches the AdministratorBrowseProfiles activity and performs necessary actions.
-     * This method clicks on the first profile in the RecyclerView, clicks on the delete button,
-     * waits for the profile to be deleted, and verifies its deletion.
-     */
-    private void launchAdministratorBrowseProfiles() {
-        // AdministratorBrowseProfiles activity
-        ActivityScenario.launch(AdministratorBrowseProfiles.class).onActivity(activity -> {
-            // wait for recyclerView
-            try {
-                Thread.sleep(8000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+    @Test
+    public void testDeleteProfile() {
+        try {
+            Thread.sleep(5000);
+        } catch (Exception ignored) {
 
-            // check if mock profile exists on the screen
-            Espresso.onView(withText("John Doe")).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        }
 
-            // click on the first item of the recyclerView
-            activity.runOnUiThread(() -> {
-                RecyclerView recyclerView = activity.findViewById(R.id.all_profiles_rv_admin);
-                RecyclerView.ViewHolder viewHolder = recyclerView.findViewHolderForAdapterPosition(0);
-                if (viewHolder != null) {
-                    viewHolder.itemView.performClick();
+        onView(withText("Mock User")).perform(ViewActions.click());
 
-                    try {
-                        Thread.sleep(5000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+        try {
+            Thread.sleep(5000);
+        } catch (Exception ignored) {
 
-                    // clicked on profile, then click yes to delete
-                    Espresso.onView(withText("Yes")).perform(ViewActions.click());
+        }
 
-                    // wait for profile to be deleted
-                    try {
-                        Thread.sleep(5000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+        onView(withText("Yes")).perform(ViewActions.click());
 
-                    // check if mock user is no longer with us
-                    Espresso.onView(withText("John Doe")).check(ViewAssertions.doesNotExist());
+        try {
+            Thread.sleep(5000);
+        } catch (Exception ignored) {
 
-                }
-            });
-        });
+        }
+
+        Espresso.onView(withText("Mock User")).check(ViewAssertions.doesNotExist());
+
     }
 }
