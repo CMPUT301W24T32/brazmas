7d238e686e328791d3750fbefb170c51c3913cc5
maywhale <68489974+maywhale@users.noreply.github.com>
2024-04-08 14:02:28 -0600

    added some changes so the parse patg diesbt crash

--- app/src/main/java/com/CMPUT301W24T32/brazmascheckin/MainActivity.java
+++ app/src/main/java/com/CMPUT301W24T32/brazmascheckin/MainActivity.java
@@ -147,7 +147,10 @@ public class MainActivity extends AppCompatActivity {
         ByteArrayOutputStream bytes = new ByteArrayOutputStream();
         inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
         String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
-        return Uri.parse(path);
+        if (path != null) {
+            return Uri.parse(path);
+        }
+        return null;
     }
 
     /**
